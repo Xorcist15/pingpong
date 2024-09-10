b9f6b477441acead25edcf819e057d5a8c1016da
youssef <xyoussefx1500@gmail.com>
2024-09-10 02:57:34 +0200

    improved layout, furthered logic

--- script.js
+++ script.js
@@ -2,60 +2,22 @@ const gameArea = document.getElementById("pingpong");
 const paddle1 = document.getElementById("left");
 const paddle2 = document.getElementById("right");
 const ball = document.getElementById("ball");
+const scoreleft = document.getElementById("scoreleft");
+const scoreright = document.getElementById("scoreright");
 
 let ballX = pingpong.clientWidth / 2;
 let ballY = pingpong.clientHeight / 2;
 
-const speed = 5;
+const speed = 1;
 let ballSpeedX = speed;
 let ballSpeedY = speed;
 
-function update() {
-
-  ballX += ballSpeedX;
-  ballY += ballSpeedY;
-
-  console.log({ ballX, ballY });
-
-  let limitX = pingpong.clientWidth - ball.clientWidth;
-  let limitY = pingpong.clientHeight - ball.clientHeight;
-
-
-  if (ballY >= limitY ||
-    ballY <= 0) {
-    ballSpeedY = -ballSpeedY;
-  }
-
-  // 
-  if (ballX >= limitX ||
-    ballX <= 0) {
-    ballSpeedX = -ballSpeedX;
-  }
-
-  // Ball collision with paddles
-  if (ballX <= paddle1.clientWidth &&
-    ballY >= paddle1.offsetTop &&
-    ballY <= paddle1.offsetTop + paddle1.clientHeight) {
-    ballSpeedX = -ballSpeedX;
-  }
-  if (ballX >= gameArea.clientWidth - paddle2.clientWidth - ball.clientWidth &&
-    ballY >= paddle2.offsetTop &&
-    ballY <= paddle2.offsetTop + paddle2.clientHeight) {
-    ballSpeedX = -ballSpeedX;
-  }
-
-
-  ball.style.left = ballX + 'px';
-  ball.style.top = ballY + 'px';
-
-  requestAnimationFrame(update);
-}
-
-requestAnimationFrame(update);
+let scoreL = 0;
+let scoreR = 0;
 
 let paddle1Y = paddle1.offsetTop;
 let paddle2Y = paddle2.offsetTop;
-const paddleSpeed = 20;
+const paddleSpeed = 1;
 
 document.addEventListener('keydown', (e) => {
   switch (e.key) {
@@ -77,3 +39,60 @@ document.addEventListener('keydown', (e) => {
       break;
   }
 });
+
+function resetBall() {
+  ballX = pingpong.clientWidth / 2;
+  ballY = pingpong.clientHeight / 2;
+}
+
+function update() {
+
+  ballX += ballSpeedX;
+  ballY += ballSpeedY;
+
+  console.log({ ballX, ballY });
+
+  let limitX = pingpong.clientWidth - ball.clientWidth;
+  let limitY = pingpong.clientHeight - ball.clientHeight;
+
+  if (ballY >= limitY ||
+    ballY <= 0) {
+    ballSpeedY = -ballSpeedY;
+  }
+
+  // Same logic used to calculate players score
+  if (ballX >= limitX) {
+    // add point player left
+    scoreL++;
+    scoreleft.innerText = scoreL;
+    console.log("Left Player scored");
+    resetBall()
+  }
+
+  if (ballX <= 0) {
+    // add point player right
+    scoreR++;
+    scoreright.innerText = scoreR;
+    console.log("Right Player scored");
+    resetBall()
+  }
+
+  // Ball collision with paddles
+  if (ballX <= paddle1.clientWidth &&
+    ballY >= paddle1.offsetTop &&
+    ballY <= paddle1.offsetTop + paddle1.clientHeight) {
+    ballSpeedX = -ballSpeedX;
+  }
+  if (ballX >= gameArea.clientWidth - paddle2.clientWidth - ball.clientWidth &&
+    ballY >= paddle2.offsetTop &&
+    ballY <= paddle2.offsetTop + paddle2.clientHeight) {
+    ballSpeedX = -ballSpeedX;
+  }
+
+  ball.style.left = ballX + 'px';
+  ball.style.top = ballY + 'px';
+
+  requestAnimationFrame(update);
+}
+
+requestAnimationFrame(update);
