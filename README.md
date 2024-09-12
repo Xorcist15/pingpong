25454e74372e0b8ddbbc50ef3ceadc62230df110
youssef <xyoussefx1500@gmail.com>
2024-09-13 00:12:57 +0200

    stable version

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 Writing pingpong game in javascript
+
+applied keyup/keydown logic to paddle movement done
+still need to fix ball/paddle collision logic done
+make ball move in more than 4 directions done
+Add better ball reset done
+make game look similar to original pong game 
+refactor
--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const gameArea = document.getElementById("pingpong");
 let ballX = pingpong.clientWidth / 2;
 let ballY = pingpong.clientHeight / 2;
 
-const speed = 7;
+const speed = 10;
 let ballSpeedX = speed;
 let ballSpeedY = speed;
 
@@ -17,83 +17,140 @@ let scoreR = 0;
 
 let paddle1Y = paddle1.offsetTop;
 let paddle2Y = paddle2.offsetTop;
-const paddleSpeed = 20;
-
-document.addEventListener('keydown', (e) => {
-  switch (e.key) {
-    case 'w':
-      paddle1Y = Math.max(paddle1Y - paddleSpeed, 0);
-      paddle1.style.top = paddle1Y + 'px';
-      break;
-    case 's':
-      paddle1Y = Math.min(paddle1Y + paddleSpeed, gameArea.clientHeight - paddle1.clientHeight);
-      paddle1.style.top = paddle1Y + 'px';
-      break;
-    case 'ArrowUp':
-      paddle2Y = Math.max(paddle2Y - paddleSpeed, 0);
-      paddle2.style.top = paddle2Y + 'px';
-      break;
-    case 'ArrowDown':
-      paddle2Y = Math.min(paddle2Y + paddleSpeed, gameArea.clientHeight - paddle2.clientHeight);
-      paddle2.style.top = paddle2Y + 'px';
-      break;
-  }
-});
+const paddleSpeed = 10;
 
 function resetBall() {
   ballX = pingpong.clientWidth / 2;
   ballY = pingpong.clientHeight / 2;
+
+  // Random angle between -45 and 45 degrees
+  const initialAngle = (Math.random() * Math.PI / 2) - (Math.PI / 4);
+  ballSpeedX = speed * Math.cos(initialAngle);
+  ballSpeedY = speed * Math.sin(initialAngle);
 }
 
-function update() {
+function calculateBounceAngle(ballY, paddleY, paddleHeight) {
+  const relativeIntersectY = (paddleY + (paddleHeight / 2)) - ballY;
+  const normalizedRelativeIntersectionY = (relativeIntersectY / (paddleHeight / 2));
+  // Max bounce angle is 45 degrees
+  const bounceAngle = normalizedRelativeIntersectionY * Math.PI / 4;
+  return bounceAngle;
+}
 
+const limitX = pingpong.clientWidth - ball.clientWidth;
+const limitY = pingpong.clientHeight - ball.clientHeight;
+
+const paddle1Limit = paddle1.offsetLeft + paddle1.clientWidth;
+const paddle2Limit = paddle2.offsetLeft - (paddle2.clientWidth / 2 + ball.clientWidth / 2);
+
+let paddle1Up = false;
+let paddle1Down = false;
+let paddle2Up = false;
+let paddle2Down = false;
+
+document.addEventListener('keydown', (e) => {
+  switch (e.key) {
+    case 'w':
+      paddle1Up = true;
+      break;
+    case 's':
+      paddle1Down = true;
+      break;
+    case 'ArrowUp':
+      paddle2Up = true;
+      break;
+    case 'ArrowDown':
+      paddle2Down = true;
+      break;
+  }
+});
+
+document.addEventListener('keyup', (e) => {
+  switch (e.key) {
+    case 'w':
+      paddle1Up = false;
+      break;
+    case 's':
+      paddle1Down = false;
+      break;
+    case 'ArrowUp':
+      paddle2Up = false;
+      break;
+    case 'ArrowDown':
+      paddle2Down = false;
+      break;
+  }
+});
+
+function update() {
   ballX += ballSpeedX;
   ballY += ballSpeedY;
 
-  console.log({ ballX, ballY });
-
-  let limitX = pingpong.clientWidth - ball.clientWidth;
-  let limitY = pingpong.clientHeight - ball.clientHeight;
-
-  if (ballY >= limitY ||
-    ballY <= 0) {
+  // logic top/bottom collision detection
+  if (ballY >= limitY || ballY <= 0) {
     ballSpeedY = -ballSpeedY;
   }
 
-  if (ballX >= limitX || ballX < 0) {
-    ballSpeedX = -ballSpeedX;
-  }
-
   // add point player left
-  if (ballX >= limitX) {
+  if (ballX >= paddle2Limit + paddle2.clientWidth) {
     scoreL++;
     scoreleft.innerText = scoreL;
-    console.log("Left Player scored");
-    resetBall()
+    resetBall();
   }
 
   // add point player right
-  if (ballX <= 0) {
+  if (ballX <= paddle1Limit - paddle1.clientWidth) {
     scoreR++;
     scoreright.innerText = scoreR;
-    console.log("Right Player scored");
-    resetBall()
+    resetBall();
   }
 
-  // Ball collision with paddles
-  if (ballX <= paddle1.clientWidth + paddle1.offsetLeft &&
-    ballY >= paddle1.offsetTop &&
-    ballY <= paddle1.offsetTop + paddle1.clientHeight) {
-    ballSpeedX = -ballSpeedX;
+  // Ball collision with left paddle 
+  if (
+    ballX <= paddle1.offsetLeft + paddle1.clientWidth &&
+    ballX + ball.clientWidth >= paddle1.offsetLeft &&
+    ballY + ball.clientHeight >= paddle1.offsetTop &&
+    ballY <= paddle1.offsetTop + paddle1.clientHeight
+  ) {
+    const bounceAngle = calculateBounceAngle(ballY + ball.clientHeight / 2, paddle1.offsetTop, paddle1.clientHeight);
+    ballSpeedX = speed * Math.cos(bounceAngle);
+    ballSpeedY = speed * -Math.sin(bounceAngle);
+    ballX = paddle1.offsetLeft + paddle1.clientWidth; // Adjust position to avoid sticking
+    ballX = paddle1.offsetLeft + paddle1.clientWidth;
   }
-  if (ballX >= gameArea.clientWidth - (paddle2.clientWidth + ball.clientWidth + (gameArea.clientWidth - (paddle2.offsetLeft + paddle2.clientWidth))) &&
-    ballY >= paddle2.offsetTop &&
-    ballY <= paddle2.offsetTop + paddle2.clientHeight) {
-    ballSpeedX = -ballSpeedX;
+
+  // Ball collision with right paddle
+  if (
+    ballX + ball.clientWidth >= paddle2.offsetLeft &&
+    ballX <= paddle2.offsetLeft + paddle2.clientWidth &&
+    ballY + ball.clientHeight >= paddle2.offsetTop &&
+    ballY <= paddle2.offsetTop + paddle2.clientHeight
+  ) {
+    const bounceAngle = calculateBounceAngle(ballY + ball.clientHeight / 2, paddle2.offsetTop, paddle2.clientHeight);
+    ballSpeedX = -speed * Math.cos(bounceAngle);
+    ballSpeedY = speed * -Math.sin(bounceAngle);
+    ballX = paddle2.offsetLeft - ball.clientWidth; // Adjust position to avoid stickingl.clientWidth;
   }
 
   ball.style.left = ballX + 'px';
   ball.style.top = ballY + 'px';
+
+  if (paddle1Up) {
+    paddle1Y = Math.max(paddle1Y - paddleSpeed, 0);
+  }
+  if (paddle1Down) {
+    paddle1Y = Math.min(paddle1Y + paddleSpeed, gameArea.clientHeight - paddle1.clientHeight);
+  }
+  paddle1.style.top = paddle1Y + "px";
+
+  if (paddle2Up) {
+    paddle2Y = Math.max(paddle2Y - paddleSpeed, 0);
+  }
+  if (paddle2Down) {
+    paddle2Y = Math.min(paddle2Y + paddleSpeed, gameArea.clientHeight - paddle2.clientHeight);
+  }
+
+  paddle2.style.top = paddle2Y + "px";
 
   requestAnimationFrame(update);
 }
